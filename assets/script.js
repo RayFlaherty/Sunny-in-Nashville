778d69a5733bce3ca938316042ea6dd9a035aa50
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-02-06 22:07:32 -0600

    let's get this started again!

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,23 @@
-var requestUrl="api.openweathermap.org/data/2.5/weather?q="+ city + "&appid=" +APIkey;
-var APIkey="78f98fdd8c2f3020975fd2b66a14d37d";
-var city="";
+const button= document.getElementById("citySearch");
+const api= "78f98fdd8c2f3020975fd2b66a14d37d";
+var city= document.getElementById("city");
+var error = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
 
-fetch(requestUrl)
+button.onclick= function cityLookUp() {
+    console.log(city.value)
 
-citySearch.onclick=function(){
-
-    var city=document.getElementById("city").value
-    var city=console.log(city)
-
+    let queryURL="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid="+api;
+    fetch(queryURL)
+    .then(error)
+    .then((response)=>{
+        console.log()
+        return response.json();
+        
+    })
+    
 }
