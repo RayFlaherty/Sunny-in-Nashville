74a221aab25aae2a5b15ac1efa21dc66de1ce853
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-03-02 18:38:32 -0600

    updating city search

--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,53 @@
 const button= document.getElementById("citySearch");
-const api= "78f98fdd8c2f3020975fd2b66a14d37d";
-var city= document.getElementById("city");
-var error = (response) => {
-    if (!response.ok) {
-        throw Error(response.statusText);
-    }
-    return response;
-}
+const geoApi= '4f8c824ff4b607b560ffd96a74610907'
+
 
 button.onclick= function cityLookUp() {
-    console.log(city.value)
+     console.log(city.value)
+    var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q="+city.value+"&limit=&appid="+geoApi;
+    axios.get(queryURL)
+    .then(function(response) {
+        if (response.ok) {
+            console.log(response);
+            response.json().then(function(data){
+                console.log(data)
+                let lat = response.data.lat;
+                let lon = response.data.lon;
 
-    let queryURL="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid="+api;
-    fetch(queryURL)
-    .then(error)
-    .then((response)=>{
-        console.log()
-        return response.json();
-        
-    })
+                console.log(lat)
+                console.log(lon)
+            })
+        }
+    }
+)}  
+    // .then(response => response.json())
+    // .then(data => console.log(data))
     
-}
+    // .catch(error => console.log(error))
+    
+    
+    
+// fetch(apiUrl)
+// .then(function(response) {
+//   // request was successful
+//   if (response.ok) {
+//     console.log(response);
+//     response.json().then(function(data) {
+//       console.log(data);
+//       displayRepos(data, user);
+//     });
+//   } else {
+//     alert("Error: " + response.statusText);
+//   }
+
+
+    // let queryURL="http://api.openweathermap.org/geo/1.0/direct?q="+city+"&limit=5&appid="+api;
+    // fetch(queryURL)
+    // .then(error)
+    // .then((response)=>{
+    //     console.log()
+    //     return response.json();
+        
+    // })
+    
+
