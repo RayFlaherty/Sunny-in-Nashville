de23d0cb33632b75ae8c3699f7b8a65552d51d6e
RayFlaherty <90649331+RayFlaherty@users.noreply.github.com>
2022-03-02 19:29:59 -0600

    weather pulls functional

--- assets/script.js
+++ assets/script.js
@@ -5,22 +5,34 @@ const geoApi= '4f8c824ff4b607b560ffd96a74610907'
 button.onclick= function cityLookUp() {
      console.log(city.value)
     var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q="+city.value+"&limit=&appid="+geoApi;
-    axios.get(queryURL)
+    fetch(queryURL)
     .then(function(response) {
         if (response.ok) {
             console.log(response);
             response.json().then(function(data){
                 console.log(data)
-                let lat = response.data.lat;
-                let lon = response.data.lon;
+                var lat = data[0].lat
+                var lon = data[0].lon
 
                 console.log(lat)
                 console.log(lon)
+
+                var currentWeather = "http://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+geoApi;
+                fetch(currentWeather)
+                    .then(function(response){
+                        if(response.ok){
+                            console.log(response);
+                            response.json().then(function(data){
+                                console.log(data)
+                            })
+                        }
+                    })
             })
         }
     }
 )}  
-    // .then(response => response.json())
+
+
     // .then(data => console.log(data))
     
     // .catch(error => console.log(error))
